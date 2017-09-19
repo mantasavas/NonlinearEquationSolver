afe43e8cbae04046e6ff384d384e386690ccf643
Mantas <mantasavas@gmail.com>
2017-09-19 22:18:48 +0300

    Adding Interface NonLinearMethod

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import NumericalMethods.NonLinearMethod;
+import NumericalMethods.NonLinearMethodFactory;
+import com.sun.deploy.security.MSCryptoDSASignature;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a test");
-        System.out.println("This is a test");
+        System.out.println("Please enter parameters of the function tan(x)");
+
+        //Creating Numerical method factory to produce methods
+        NonLinearMethodFactory nonlinerFactor = new NonLinearMethodFactory();
+
+        // Calculating with the method Fixed point iteration
+        NonLinearMethod fixedPoint = nonlinerFactor.getNonLinearMethod("FixedPoint");
+        fixedPoint.calculate();
+
+        // Calculating with the method Secant
+        NonLinearMethod secant = nonlinerFactor.getNonLinearMethod("Secant");
+        secant.calculate();
+
     }
+
+
 }
--- src/NumericalMethods/NonLinearMethod.java
+++ src/NumericalMethods/NonLinearMethod.java
@@ -1,4 +1,25 @@
 package NumericalMethods;
 
-public class NonLinearMethod {
+/**
+ * <h1>Add Two Numbers!</h1>
+ * The AddNum program implements an application that
+ * simply implements main algorithm for calculating equations based on fixed point iteration and secant methods
+ *
+ * @author  Mantas Savaniakas
+ * @version 1.0
+ * @since   2017-09-19
+ */
+
+public interface NonLinearMethod {
+
+    /**
+     * This method is used to add two integers. This is
+     * a the simplest form of a class method, just to
+     * show the usage of various javadoc Tags.
+     * @param numA This is the first paramter to addNum method
+     * @param numB  This is the second parameter to addNum method
+     * @return int This returns sum of numA and numB.
+     */
+
+    void calculate(); // Main algorithm stored
 }
